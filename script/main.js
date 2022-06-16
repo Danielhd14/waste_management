7baaa2f21daaf6ddb1d8ad83e684210d5ab9a3f3
harry.daniel <danielharry353@gmail.com>
2022-06-16 11:39:13 +0100

    LGA select tag

---
+++ script/main.js
@@ -0,0 +1,16 @@
+// LGA select tag start
+let lGA = document.getElementById('LGA')
+let url = "../script/api.json"
+async function LocalGovernment(){
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data[0].Rivers)
+    for(let i = 0; i < data[0].Rivers.length;i++){
+        lGA.innerHTML +=`
+        <option>${data[0].Rivers[i]}</option>    
+        `
+    }
+}
+LocalGovernment()
+// LGA select tag end
+
